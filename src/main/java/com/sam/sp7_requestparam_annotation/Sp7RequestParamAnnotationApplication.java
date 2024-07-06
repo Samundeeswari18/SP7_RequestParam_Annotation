6e868aa9ca70166c7f52d087eaf991e1463188cf
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 14:40:10 -0400

    Request param controller

---
+++ src/main/java/com/sam/sp7_requestparam_annotation/Sp7RequestParamAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sp7_requestparam_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp7RequestParamAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp7RequestParamAnnotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/sp7_requestparam_annotation/Sp7RequestParamAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sp7_requestparam_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp7RequestParamAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
